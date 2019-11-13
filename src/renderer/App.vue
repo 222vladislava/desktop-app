3be65a0902041e15e248733d88119f2791debea7
Vladislava Ma <222vladislava@gmail.com>
2019-11-13 23:17:31 +0200

    Add Index page and menu

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -10,6 +10,80 @@
   }
 </script>
 
-<style>
-  /* CSS */
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,700|Roboto:300,400,500,700&display=swap');
+
+  * {
+    box-sizing: border-box;
+  }
+
+  h1, h2, h3, h4, h5 {
+    font-family: 'Roboto', sans-serif;
+  }
+
+  p, span, div, ul, a {
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  body {
+    margin: 0;
+  }
+
+  .page {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+
+    &__menu {
+      width: 300px;
+      border-right: 1px solid #d8d8d8;
+
+      &__logo {
+        border-bottom: 1px solid #d8d8d8;
+
+        h1 {
+          margin: 0;
+          font-size: 19px;
+          text-align: center;
+          padding: 12px 0px;
+        }
+      }
+
+      ul {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+
+        > li {
+          margin-top: 15px;
+        }
+
+        li {
+          a {
+            color: black;
+            display: flex;
+            padding-left: 25px;
+          }
+
+          ul {
+            margin-left: 20px;
+            li {
+              a {
+                text-decoration: none;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    &__content {
+      width: 100%;
+      padding: 10px 30px;
+
+      &__text {
+
+      }
+    }
+  }
 </style>
--- src/renderer/components/pages/Index.vue
+++ src/renderer/components/pages/Index.vue
@@ -1,6 +1,54 @@
 <template>
   <div>
-    Index page
+    <div class="page">
+      <div class="page__menu">
+        <div class="page__menu__logo">
+          <h1>Хиромантия</h1>
+        </div>
+        <ul>
+          <li>
+            <router-link to="/">Линии</router-link>
+
+            <ul>
+              <li>
+                <router-link to="/">Холмы</router-link>
+              </li>
+              <li>
+                <router-link to="/">Холмы</router-link>
+              </li>
+              <li>
+                <router-link to="/">Холмы</router-link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <router-link to="/">Холмы</router-link>
+
+            <ul>
+              <li>
+                <router-link to="/">Холмы</router-link>
+              </li>
+              <li>
+                <router-link to="/">Холмы</router-link>
+              </li>
+              <li>
+                <router-link to="/">Холмы</router-link>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div class="page__content">
+        <div class="page__content__text">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <p>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
